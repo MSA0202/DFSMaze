adb8db44b5cf2b2be6123dd789e1f2e529ace0f1
Muhammad Saif Alli <muhammadsaifalli222@gmail.com>
2024-03-16 11:37:46 +0200

    Added codr for printinng route and testing it, also some stub code for moving logic and dfs logic

--- dfs.py
+++ dfs.py
@@ -1,10 +1,7 @@
 class dfs:
-    #Start at S and goal is E
-    #Able to move up, right, left down
-    #need function to define movement
-    #need to record previous moves
+
     def __init__(self):
-        self.recordMoves = []
+        self.recordMoves = [[1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5], [5, 6], [5, 7], [5, 8], [6, 8], [7, 8], [8, 8]] #for testing, put as empty after
 
     def printMaze(self,maze):
         for row in maze:
@@ -16,29 +13,42 @@ class dfs:
         for row in range(len(maze)):
             for column in range(len(maze[row])):
                 if maze[row][column] == 'S':
-                    return row,column #x,y coords
+                    return [row,column] #x,y coords
                 
     def goalPosition(self,maze):
         for row in range(len(maze)):
             for column in range(len(maze[row])):
                 if maze[row][column] == 'E':
-                    return row,column #x,y coords
+                    return [row,column] #x,y coords
                     
-    def up(self,currentRow,currentColumn,maze):
+    def move(self,currentRow,currentColumn,maze):
         moveUp = currentRow - 1
-        if maze[moveUp][currentColumn] != '#':
-            self.recordMoves.append([moveUp,currentColumn])
+        moveRight = currentColumn + 1
+        moveDown = currentRow + 1
+        moveLeft = currentColumn - 1
 
-    # def right(self,currentCoords):
+        if maze[moveUp][currentColumn] != '#' and ([moveUp,currentColumn] not in self.recordMoves): #and perhaps need to check if this move is not already recorded
+            self.recordMoves.append([moveUp,currentColumn]) # append coordinates
+        elif maze[currentRow][moveRight] != '#'  and ([currentRow,moveRight] not in self.recordMoves):
+            self.recordMoves.append([currentRow,moveRight])
+        elif maze[moveDown][currentColumn] != '#' and ([moveDown,currentColumn] not in self.recordMoves):
+            self.recordMoves.append([moveDown,currentColumn])
+        elif maze[currentRow][moveLeft] != '#' and ([currentRow,moveLeft] not in self.recordMoves):
+            self.recordMoves.append([currentRow,moveLeft])
 
-    # def down(self,currentCoords):
-
-    # def left(self,currentCoords):
-    
-    # def DFS(self, maze):
-    #     recordMoves = []
-    #     #
-
+    def printRoute(self,maze):
+        for row in range(len(maze)):
+            for column in range(len(maze)):
+                print([row,column])
+                if [row,column] in self.recordMoves and [row,column] != self.startingPosition(maze) and [row,column] != self.goalPosition(maze):
+                    maze[row][column] = '*'
+                
+    def DFS(self, maze):
+        while self.goalPosition() not in self.recordMoves:
+            self.move()
+        self.printRoute()
+        self.printMaze()
+        
 
 def main():
     maze1 = [
@@ -79,8 +89,10 @@ def main():
     ]
 
     init = dfs()
+    #init.printMaze(maze1)
+    #print(init.startingPosition(maze1))
+    #print(type(init.startingPosition(maze1)))
+    init.printRoute(maze1)
     init.printMaze(maze1)
-    print(init.startingPosition(maze1))
-
 if __name__ == '__main__':
     main()
