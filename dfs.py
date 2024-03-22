89e132d65a2dd7842798c3f6ca56b53890f0985e
Muhammad Saif Alli <muhammadsaifalli222@gmail.com>
2024-03-22 11:02:39 +0200

    Implemented the dfs function and update the move fucntion to backtrack when blocked an dno where to go

--- dfs.py
+++ dfs.py
@@ -1,7 +1,8 @@
 class dfs:
 
     def __init__(self):
-        self.recordMoves = [[1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5], [5, 6], [5, 7], [5, 8], [6, 8], [7, 8], [8, 8]] #for testing, put as empty after
+        self.recordMoves = [] #for testing, put as empty after
+        self.visited = []
 
     def printMaze(self,maze):
         for row in maze:
@@ -27,27 +28,36 @@ class dfs:
         moveDown = currentRow + 1
         moveLeft = currentColumn - 1
 
-        if maze[moveUp][currentColumn] != '#' and ([moveUp,currentColumn] not in self.recordMoves): #and perhaps need to check if this move is not already recorded
+        if maze[moveUp][currentColumn] != '#' and ([moveUp,currentColumn] not in self.visited):
             self.recordMoves.append([moveUp,currentColumn]) # append coordinates
-        elif maze[currentRow][moveRight] != '#'  and ([currentRow,moveRight] not in self.recordMoves):
+            self.visited.append([moveUp,currentColumn])
+        elif maze[currentRow][moveRight] != '#'  and ([currentRow,moveRight] not in self.visited):
             self.recordMoves.append([currentRow,moveRight])
-        elif maze[moveDown][currentColumn] != '#' and ([moveDown,currentColumn] not in self.recordMoves):
+            self.visited.append([currentRow,moveRight])
+        elif maze[moveDown][currentColumn] != '#' and ([moveDown,currentColumn] not in self.visited):
             self.recordMoves.append([moveDown,currentColumn])
-        elif maze[currentRow][moveLeft] != '#' and ([currentRow,moveLeft] not in self.recordMoves):
+            self.visited.append([moveDown,currentColumn])
+        elif maze[currentRow][moveLeft] != '#' and ([currentRow,moveLeft] not in self.visited):
             self.recordMoves.append([currentRow,moveLeft])
+            self.visited.append([currentRow,moveLeft])
+        else: #backtrack to previous move
+            self.recordMoves.pop()
 
     def printRoute(self,maze):
         for row in range(len(maze)):
             for column in range(len(maze)):
-                print([row,column])
                 if [row,column] in self.recordMoves and [row,column] != self.startingPosition(maze) and [row,column] != self.goalPosition(maze):
                     maze[row][column] = '*'
                 
     def DFS(self, maze):
-        while self.goalPosition() not in self.recordMoves:
-            self.move()
-        self.printRoute()
-        self.printMaze()
+        currentRow = self.startingPosition(maze)[0]
+        currentColumn = self.startingPosition(maze)[1]
+        while self.goalPosition(maze) not in self.recordMoves:
+            self.move(currentRow,currentColumn,maze)
+            currentRow = self.recordMoves[len(self.recordMoves)-1][0] #because we always want to be at the most recently added position that has been recorded
+            currentColumn = self.recordMoves[len(self.recordMoves)-1][1]
+        self.printRoute(maze)
+        self.printMaze(maze)
         
 
 def main():
@@ -89,10 +99,14 @@ def main():
     ]
 
     init = dfs()
-    #init.printMaze(maze1)
-    #print(init.startingPosition(maze1))
-    #print(type(init.startingPosition(maze1)))
-    init.printRoute(maze1)
-    init.printMaze(maze1)
+    print("MAZE1")
+    init.DFS(maze1)
+    print("MAZE2")
+    init.DFS(maze2)
+    print("MAZE3")
+    init.DFS(maze3)
 if __name__ == '__main__':
     main()
+
+
+# self.recordMoves  for maze1= [[1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5], [5, 6], [5, 7], [5, 8], [6, 8], [7, 8], [8, 8]] #for testing, put as empty after
